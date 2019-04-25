a64293d87ccec8f3840b0d48458448a1f654a52c
Alison Williams <alison.lauriewill@gmail.com>
2019-04-25 21:05:32 +0100

    Vets and Animals tables created in DB

---
+++ db/clinic.sql
@@ -0,0 +1,19 @@
+DROP TABLE animals;
+DROP TABLE vets;
+
+
+CREATE TABLE vets
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null
+);
+
+CREATE TABLE animals
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+  date_of_birth VARCHAR(255),
+  type VARCHAR(255),
+  treatment_notes VARCHAR(255),
+  vet_id INT8 references vets(id)
+);
