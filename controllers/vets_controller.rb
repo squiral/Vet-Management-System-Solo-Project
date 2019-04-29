e49f5dc9ebb152812cd05976069e6d2b3d0a3987
Alison Williams <alison.lauriewill@gmail.com>
2019-04-29 11:35:12 +0100

    Vet delete function and views added

--- controllers/vets_controller.rb
+++ controllers/vets_controller.rb
@@ -15,7 +15,7 @@ end
 #new
 
 get '/vets/new' do
-  @animals = Animal.all()
+  @unassigned_animals = Animal.unassigned()
   erb(:"vets/new")
 end
 
@@ -30,6 +30,17 @@ end
 
 post '/vets' do
   @vet = Vet.new( params )
+  @animal = Animal.find( params )
+  @animal.vet_id = @vet.id
+  @animal.update
   @vet.save
   erb(:"vets/create")
+end
+
+#destroy
+
+post '/vets/:id/delete' do
+  @vet = Vet.find( params[:id] )
+  @vet.delete()
+  erb(:"vets/destroy")
 end
